edf05fc6ea0c8020145faaf647041d9d7a410baf
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2022-02-19 14:05:26 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SafeArea(
         child: Column(
           children: [
             Text("dark theme and light theme")
